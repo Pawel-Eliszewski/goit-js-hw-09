0602d285953b5eb1b0a9eef0a137c74f77098e11
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-03-13 11:31:25 +0100

    HW 09 v2.0

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,28 +2,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const timer = document.querySelector('.timer');
-timer.style.display = 'flex';
-
-const field = document.querySelectorAll('.field');
-for (const label of field) {
-  label.style.cssText = `
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 10px;
-  font-size: 11px;
-  text-transform: uppercase;
-`;
-}
-
-const span = document.querySelectorAll('span.value');
-for (const value of span) {
-  value.style.fontSize = '24px';
-}
-
 const dateInput = document.querySelector('#datetime-picker');
-dateInput.style.marginLeft = '9px';
 const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
 
@@ -69,6 +48,8 @@ const options = {
                 hours.textContent = addLeadingZero(timeLeft.hours);
                 minutes.textContent = addLeadingZero(timeLeft.minutes);
                 seconds.textContent = addLeadingZero(timeLeft.seconds);
+              } else {
+                dateInput.disabled = false;
               }
             }
           }
